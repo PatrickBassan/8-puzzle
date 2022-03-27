15f1e7a61194f6f87f5a7c4985e49176bec2459a
PatrickBassan <patrickfbassan@gmail.com>
2022-03-27 16:35:58 -0300

    Added function to check if puzzle is solvable

--- solver.cpp
+++ solver.cpp
@@ -11,11 +11,11 @@
 
 using namespace std;
 
-map<vector<vector<int> > , bool> visited; //declaring a map named "visited" that takes vector(which stores vectors that takes integer values) as its key, and a boolean as its value
-map<vector<vector<int> > , vector<vector<int> > > parent; //declaring a map named "parent" that takes vectors as its both key and value to keep track of the nodes
+map<vector<vector<int> > , bool> visited; //mapa "visited" que pega vetor(que guarda vetores do tipo int) como chaves,a boolean como valor
+map<vector<vector<int> > , vector<vector<int> > > parent; //mapa "parent" que pega vetoes como chave e valor para manter controle dos nós
 vector<vector<int> > goal(3,vector<int> (3)); 
 
-bool visit(vector<vector<int> > a)  //boolean to see if node is visited
+bool visit(vector<vector<int> > a)  //boolean para ver se o nó foi visitado
 {
 	if(visited[a]==true)
 		return true;
@@ -24,11 +24,11 @@ bool visit(vector<vector<int> > a)  //boolean to see if node is visited
 }
 
 
-//Manhattan or A*(A-star) search to find the distance 
+//Manhattan para achar a distancia
 int manhattan (vector<vector<int> > a , int moves)
 {
 	int dist = moves;
-	for (int i=0; i<3; i++) //for loop to go through each row and column (like matrices). [00,01,02,10,11,12,20,21,22] We'll be using this i,j loop more.
+	for (int i=0; i<3; i++) //loop para passar em cada linha e coluna
 	{
 		for (int j=0; j<3; j++)
 		{
@@ -39,7 +39,7 @@ int manhattan (vector<vector<int> > a , int moves)
 					for (int l=0; l<3; l++)
 					{
 						if(a[i][j]==goal[k][l])
-							dist+=abs(i-k)+abs(j-l); //absolute value since it may be negative
+							dist+=abs(i-k)+abs(j-l); //valor absoluto pois pode ser negativo
 					}
 				}
 			}
@@ -48,12 +48,12 @@ int manhattan (vector<vector<int> > a , int moves)
 	return dist;	
 }
 
-bool isGoal (vector<vector<int> > a){  //boolean to see if we've reached our goal state
+bool isGoal (vector<vector<int> > a){  //boolean para verificar se chegou no goal
 	for(int i=0; i<3; i++)
 	{
 		for(int j=0; j<3; j++)
 		{
-			if(a[i][j] != goal[i][j]) return 0; //if our puzzle tiles aren't same with the goal position. For eg. we want a[0][0] = 1, a[0][1] = 2, a[2][2] = 0, etc.
+			if(a[i][j] != goal[i][j]) return 0; //checa se os quadrados  estao na mesma posicao da goal
 			
 		}
 	}
@@ -61,31 +61,20 @@ bool isGoal (vector<vector<int> > a){  //boolean to see if we've reached our goa
 	return 1;	
 }
 
-bool safe(int i,int j) //boolean safe to see if grid is 3x3
+bool safe(int i,int j) //boolean para ver se a grid é 3x3
 {
 	if(i>=0 && i<=2 && j>=0 && j<=2)
 		return true;
 	
 	return false;
 }
-/*
-bool check(int i, int, j) //checa se eh posivel solucionar
-{
-	//pega o tamanho N do tabuleiro
-	//for pra pegar o numero de inversoes
-	//valida se N é par ou impar
-	//se N for impar e inversao for impar, nao pode resolver, senao pode
-	//se N for par, inversao + linha do quadrado branco, result igual a par, nao consegue resolver, result igual a impar, consegue resolver 
-}
-*/
 
-
-int dx[]={-1,1,0,0}; //array named "dx", with 4 length
+int dx[]={-1,1,0,0}; //array "dx", com tamanho 4
 int dy[]={0,0,1,-1};
 
 vector<vector<vector<int> > > neighbours(vector<vector<int> > a) 
 {
-	pair<int,int> pos; //vector of pairs named "pos" (pairs an integer to another integer)
+	pair<int,int> pos; //vetor de pares "pos" (emparelha um inteiro com outro inteiro)
 	for (int i=0; i<3; i++)
 	{
 		for (int j=0; j<3; j++){
@@ -99,28 +88,28 @@ vector<vector<vector<int> > > neighbours(vector<vector<int> > a)
 		
 	}
 	
-	vector<vector<vector<int> > > ans; //"answer" vector to add the correct values by pushing back
-	for (int k=0; k<4; k++) // 4, not 3, since our dx and dy arrays have 4 length
+	vector<vector<vector<int> > > ans; //vetor "answer" que add os valores corretos dando push back
+	for (int k=0; k<4; k++) // 4, nao 3, pois os vetores dx e dy tem tamanho 4
 	{
-		int cx = pos.first; //to keep track of the "i" values
-		int cy = pos.second; //to keep track of the "j" values
+		int cx = pos.first; //para manter o controle dos valores "i"
+		int cy = pos.second; //para manter o controle dos valores "j"
 		vector<vector<int> > n = a;
 		if(safe(cx+dx[k], cy+dy[k]))
 		{
-			swap(n[cx+dx[k]][cy+dy[k]], n[cx][cy]); //swap tiles(or we can say swap arrays)
+			swap(n[cx+dx[k]][cy+dy[k]], n[cx][cy]); //troca posição dos quadrados(arrays)
 			ans.push_back(n);			
 		}
 	}
 		
-	return ans; //result vector
+	return ans; //vetor resultado
 		
 }
 
-typedef pair<vector<vector<int> > , int> state; // new type defined named "state" that's a vector of pairs (which is a vector to integer pair)
+typedef pair<vector<vector<int> > , int> state; // define novo tipo "state" que é um vetor de pares
 	
 struct cmp 
 {
-	bool operator() (state &a, state &b) // overloading the unary operator "()"
+	bool operator() (state &a, state &b) // sobrecarregando o operador unário "()"
 	{
 		int am = manhattan(a.first,a.second);
 		int bm = manhattan(b.first,b.second);
@@ -130,8 +119,8 @@ struct cmp
 
 void print_path(vector<vector<int> > s)
 {
-	if(parent.count(s)) //to see if our vector s is present in our map named "parent" which takes vectors in it for both key and value, and if it is present
-	print_path(parent[s]); //then print the path
+	if(parent.count(s)) //checa se o vetor s esta presente no mapa "parent" que recebe vetores com chave e valores, se estiver presente
+	print_path(parent[s]); //imprime o caminho
 	
     for(int i=0;i<3;i++)
     {
@@ -162,7 +151,7 @@ void print(vector<vector<int> > s)
 void solve(vector<vector<int> > a, int moves)
 {
 	
-	priority_queue<state, vector<state>, cmp> Q; //Priority queue named "Q". 
+	priority_queue<state, vector<state>, cmp> Q; //Fila de prioridade "Q". 
 	Q.push(state(a,moves));
 	while(!Q.empty())
 	{
@@ -189,32 +178,55 @@ void solve(vector<vector<int> > a, int moves)
 	return;
 }
 
+bool solvable(vector<vector<int> > a, vector<int> b)
+{
+	//se N for impar e inversao for impar, nao pode resolver, senao pode
+	int inv = 0;
+	for(int i=0;i<b.size();i++)
+	{
+		if (b[i] != 0) {
+			for(int j=i;j<b.size();j++)
+			{
+				if (b[i] > b[j] && b[j] != 0){
+					inv += 1;
+				}
+			}
+		}
+	}
+	cout<<"\ninversoes: "<<inv;
+	if (inv % 2 == 0) {
+		cout<<"\nPuzzle com solucao, aguarde...\n";
+		solve(a,0);
+	} else {
+		cout<<"\nPuzzle sem solucao\n";
+		return false;
+	}
+}
+
 int main()
 {
-
-  string line;
-  ifstream myFile("prog1_input.txt");
-  if (myFile.is_open())
-  {
-    while ( getline( myFile, line))
-    {
-     // cout << line << '\n';d
-    }
-    myFile.close();
-  }
-
-
+	//1 2 3 4 5 6 8 7 0 (unsolvable)
+	//1 3 2 5 0 6 7 8 4 (unsolvable)
+	//0 1 3 4 2 5 7 8 6 (solvable)
 	cout<<"Digite o puzzle do 8 como string! \n";
 	cout<<"\nPor exemplo => 0 1 3 4 2 5 7 8 6\n";
-	vector<vector<int> > a(3,vector<int> (3));	
+	vector<vector<int> > a(3,vector<int> (3));
+	vector<int> b(9);
+	
 	for(int i=0;i<3;i++)
 	{
 		for(int j=0;j<3;j++)
 		{
-			cin>>a[i][j];	
+			//recebe dados digitados
+			cin>>a[i][j];
 		}
 	}
-	cout<<"\nAguarde...\n";
+
+	for(int x=0;x<b.size();x++)
+	{
+		cin>>b[x];	
+	}
+	
 	goal[0][0] = 1;
 	goal[0][1] = 2;
 	goal[0][2] = 3;
@@ -224,10 +236,6 @@ int main()
 	goal[2][0] = 7;
 	goal[2][1] = 8;
 	goal[2][2] = 0;
-	solve(a,0);
-
-	ofstream outFile;
-	outFile.open("prog1_output.txt");
-
+	solvable(a, b);
 }
 
